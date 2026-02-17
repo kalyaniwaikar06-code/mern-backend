071e7b3ac9b755222b1c590f6e9a28b961940922
Kalyani Waikar <kalyaniwaikar06@gmail.com>
2026-02-17 18:51:41 +0530

    Updated API baseURL to production

--- frontend/src/api/userApi.js
+++ frontend/src/api/userApi.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://mern-backend-oisx.onrender.com/api",
 });
 
 export const getUsers = () => API.get("/users");
 export const createUser = (data) => API.post("/users", data);
-// export const updateUser = (id, data) => API.put(`/users/${id}`, data); // later
-// export const deleteUser = (id) => API.delete(`/users/${id}`);
+export const updateUser = (id, data) => API.put(`/users/${id}`, data);
+export const deleteUser = (id) => API.delete(`/users/${id}`);
